dd9876712a417f5d3a53760d780a709d200a30a8
kshuta <shutakumada@gmail.com>
2021-08-27 14:56:15 -0500

    created arbitrary test data

--- server.go
+++ server.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	workouts := getTestWorkouts()
+	fmt.Fprint(w, workouts[0].Name)
+}
 
 func main() {
 	fmt.Println("hello from server")
-	fmt.Println(workout)
+
+	mux := httprouter.New()
+	mux.GET("/", Index)
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
--- test_data.go
+++ test_data.go
@@ -6,10 +6,158 @@ import (
 	"github.com/kshuta/workout_tracker/data"
 )
 
-var workout data.Workout = data.Workout{
-	Id:        1,
-	Name:      "test-workout",
-	WeekNo:    1,
-	Date:      time.Now(),
-	CreatedAt: time.Now(),
+func getTestWorkouts() []data.Workout {
+	workouts := []data.Workout{
+		{
+			Id:        1,
+			Name:      "test-workout",
+			WeekNo:    1,
+			Date:      time.Now(),
+			CreatedAt: time.Now(),
+		},
+	}
+
+	return workouts
+}
+
+func getTestLifts() []data.Lift {
+	lifts := []data.Lift{
+		{
+			Id:        1,
+			Name:      "Bench Press",
+			Max:       100,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        2,
+			Name:      "Squat",
+			Max:       150,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        3,
+			Name:      "Deadlifts",
+			Max:       200,
+			CreatedAt: time.Now(),
+		},
+	}
+
+	return lifts
+}
+
+func getTestSets() []data.Set {
+	sets := []data.Set{
+		{
+			Id:        1,
+			LiftId:    1,
+			Done:      false,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        2,
+			LiftId:    1,
+			Done:      false,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        3,
+			LiftId:    1,
+			Done:      false,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        4,
+			LiftId:    2,
+			Done:      false,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        5,
+			LiftId:    2,
+			Done:      false,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        6,
+			LiftId:    3,
+			CreatedAt: time.Now(),
+		},
+		{
+			Id:        7,
+			LiftId:    3,
+			CreatedAt: time.Now(),
+		},
+	}
+
+	return sets
+}
+
+func getSetQuantity() []data.SetQuantity {
+	sqs := []data.SetQuantity{
+		{
+			Id:           1,
+			SetId:        1,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 60,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           2,
+			SetId:        2,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 70,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           3,
+			SetId:        3,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 75,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           4,
+			SetId:        4,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 65,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           5,
+			SetId:        5,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 75,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           6,
+			SetId:        6,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 80,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+		{
+			Id:           7,
+			SetId:        7,
+			Reptype:      data.Count,
+			Quantity:     8,
+			PlannedRatio: 85,
+			Ratiotype:    data.Percentage,
+			CreatedAt:    time.Now(),
+		},
+	}
+
+	return sqs
 }
